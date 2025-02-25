e3d5a1f362091f6e51a80fe498db398b6b040a06
Kim Dionisio <kimdionisio4@gmail.com>
2025-02-25 11:49:07 +0800

    Evaluating Student Grades with Switch and Loops

---
+++ Loop-Based Programming/Evaluate.cs
@@ -0,0 +1,30 @@
+using System;
+					
+public class Evaluate
+{
+    public static void Main()
+    {
+        int[] scores = { 95, 82, 75, 63, 58 };
+
+		for (int i = 0; i < scores.Length; i++) {
+			int score = scores[i];
+			switch (score) {
+				case int n when (n >= 90):
+					Console.WriteLine("Grade A: Excellent!");
+					break;
+				case int n when (n >= 80):
+					Console.WriteLine("Grade B: Good job!");
+					break;
+				case int n when (n >= 70):
+					Console.WriteLine("Grade C: Fair.");
+					break;
+				case int n when (n >= 60):
+					Console.WriteLine("Grade D: Needs improvement.");
+					break;
+				default:
+					Console.WriteLine("Grade F: Fail.");
+					break;
+			}
+		}
+    }
+}
