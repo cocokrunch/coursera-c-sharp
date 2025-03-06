2ee25ff922feea8ed03b677caac36b9dfa0357cb
Kim Dionisio <kimdionisio4@gmail.com>
2025-03-06 22:06:25 +0800

    Practical Application – User Age Validation

---
+++ AgeValidation.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class AgeValidation
+{
+
+	public static void Main() {
+		// Call the method
+		Console.WriteLine("Enter your age: ");
+		int age = int.Parse(Console.ReadLine());
+		bool canDrive = IsOldEnoughToDrive(age);
+		if (canDrive) {
+			Console.WriteLine("You are old enough to drive.");
+		} else {
+			Console.WriteLine("You are not old enough to drive.");
+		}	
+	}
+	// Method Definition
+	static bool IsOldEnoughToDrive(int age) {
+		if (age >= 18) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
